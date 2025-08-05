fd9d2f542263dcc9583e5bea17382cdb887b4d52
FebryanHernanda <febryanhernanda@gmail.com>
2025-08-06 00:38:08 +0700

    feat(main): add todolistprovider fragment

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./Routes.jsx";
 import AuthLoginProvider from "./context/AuthLogin/AuthLoginProvider.jsx";
+import TodoListProvider from "./context/TodoList/TodoListProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthLoginProvider>
-      <Router />
+      <TodoListProvider>
+        <Router />
+      </TodoListProvider>
     </AuthLoginProvider>
   </StrictMode>
 );
